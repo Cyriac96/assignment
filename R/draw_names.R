03f32e7abc4be31dd8180dc266b2ff6e8285c2dc
Cyriac96 <cyriacparisot@gmail.com>
2017-10-16 16:29:53 +0200

    Creation of the function draw_names

---
+++ R/draw_names.R
@@ -0,0 +1,23 @@
+#' Function: draw_names_dygraph
+#' This function plots the occurence of two names over time
+#' @param names noms recherchés
+#'
+#' @return graph
+#' @import prenoms
+#' @import ggplot2
+#' @import dplyr
+#' @import dygraphs
+#' @import tidyr
+#' @export
+#'
+#' @examples
+#' draw_names_dygraph(names = "Pierre")
+#'
+draw_names_dygraph <- function(names){
+  prenoms::prenoms %>%
+    group_by(year, name) %>%
+    summarise (total = sum(n)) %>%
+    filter (name %in% names) %>%
+    spread(key = name, value = total) %>%
+    dygraph()
+}
